b47d489d6a5a731dee223f6959d1a104a7817c5d
Misha <31502901+MPeaches@users.noreply.github.com>
2020-07-02 18:37:30 +0300

    large update
    Now can fetch images from VK posts and is easier to set up

--- README.md
+++ README.md
@@ -1,17 +1,16 @@
-An another super barebones color bot. It can:  
+This is a color bot with an extra twist. It can:
 * Create color roles for users
 * Change color of user's color role
 * Clean users from color
-* Enable/disable roles
+* Toggle user roles per their request
+* Fetch images from Vkontakte posts
 
 Stock commands:
 * !setchannel - elect channel, where bot would operate
 * !robl - help
 * !colorme <hexcode\> or \<random> - change color
 * !cleanme - delete color
-* !toggle \<role> - to add/delete user role
+* !toggle \<role> - to add/delete role to a user
 * !cleancolors (admin only) - delete all color roles from server
 
-To launch bot, past your app token instead of 'token' placehoder, install nodejs and do node r.js in bot's directory.
-
-I made this bot mainly for myself, but if you interested in simple bot with this set functions, give me notice, I would translate it and add ability to save remeber channels.
+To launch your bot, install all dependencies, fill the initInfo json file with guilds and channels where it should work, paste your API key and start the bot.
--- r.js
+++ r.js
@@ -1,26 +1,107 @@
 const Discord = require('discord.js');
+const initInfo = require('initInfo.json')
 const client = new Discord.Client();
-var chnls = new Map();
+const request = require("request")
+const cheerio = require("cheerio")
+const util = require("util")
+const vkLinkRgxp = /https:\/\/vk\.com\/wall-[^\s\?а-яa-z]*/gm
+const vkReplyRgxp = /https:\/\/vk\.com\/wall-.*[0-9]_.*[0-9]\?reply=[^\s\?а-яa-z]*/gm
 
+const requestOptions = {
+  url: '',
+  headers: {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.121 Safari/537.36 Vivaldi/2.8.1664.44',
+    'Accept-Charset': 'utf-8'
+  }
+}
+
+const someoneTemplates = [
+  "(◕‿◕✿)",
+  "（✿ ͡◕ ᴗ◕)つ━━✫・o。",
+  "(╯°□°）╯︵ ┻━┻",
+  "ヽ༼ ಠ益ಠ ༽ﾉ",
+  "¯\\_(ツ)_/¯",
+  "(⁄ ⁄•⁄ω⁄•⁄ ⁄)",
+  "ಠ_ಠ",
+  "༼ つ ◕_◕ ༽つ",
+  "(∩ ͡° ͜ʖ ͡°)⊃━☆ﾟ. o ･ ｡ﾟ",
+]
+
+var chnls = new Map();
+initInfo.permChannels.forEach(e => {
+  chnls.set(e);
+});
 client.on('ready', () => {
   console.log('I am ready!');
 });
 
-client.on('error', err => {
-	if (err.code !== 'ECONNRESET'){throw err}
-})
-
-client.on('uncaughtException', e => {})
+client.on('uncaughtException', e => {return})
 
 client.on('message', message => {  
   if (!message.guild) return;
   const mmbr = message.guild.member(message.author);
   if (mmbr == client.user) return;
 
-  if (message.content.startsWith('!setchannel') && (mmbr.hasPermission('ADMINISTRATOR'))) {
-    chnls.set(message.guild.id, message.channel.id);
-    message.channel.send(`Канал выбран, напиши !robl для помощи`);
-  };
+ // if (message.content.startsWith("@someone")){
+ //   message.channel.send(someoneTemplates[Math.floor(Math.random()*someoneTemplates.length)] +
+ //   ` (${message.guild.members.random().displayName}) ` + message.content.substring(9));
+ // }
+
+  if (vkLinkRgxp.test(message.content)){
+  let lir = [], zhopa = [], firstImage;
+  requestOptions.url =  message.content.match(vkLinkRgxp)[0]
+  const promisifiedReq = util.promisify(request)
+  promisifiedReq(requestOptions).then(
+    (fuck) => {
+      var $ = cheerio.load(fuck.body)
+      if ($(".message_page_body").length > 0) {
+        message.channel.send(`Страница скрыта`); 
+        return;
+      }
+      if ($(".copy_post_img").length > 0) {
+        message.channel.send(`Репосты не поддерживаются`);
+        return;
+      }
+      $(".wall_post_cont .page_post_thumb_wrap")
+      .each((index, element) => {
+         lir.push($(element).attr("onclick"));
+      })
+      zhopa = getVKLinks(lir);
+      return promisifiedReq(requestOptions.url)
+    }).then((fuck) => {
+      let $ = cheerio.load(fuck.body)
+  
+    const ava = $(".wi_img").attr("src"),
+          authorName = $(".pi_author").text(),
+          postText = $(".pi_text").text();
+    ava.length -= 6;
+    if (zhopa.length == 0) zhopa[0]='';
+  
+    const vkPostInfo = {embed:{
+      author: {
+        name: authorName, 
+        icon_url: ava
+      },
+      description: postText,
+      image: {
+        url: zhopa[0] 
+      } 
+    }
+    }
+
+    if (zhopa.length == 0 || zhopa.length == 1) {
+      message.channel.send(vkPostInfo)
+    } else if (zhopa.length > 1) {
+      message.channel.send(vkPostInfo).then(() => {
+        zhopa.shift()
+        zhopa.join('\n')
+        message.channel.send(zhopa.splice(0, 4)).then(() => {
+          if (zhopa.length > 1) message.channel.send(zhopa)}
+              );
+        });
+	}
+    }).catch(e => console.log(e))
+  }
 
   if (chnls.get(message.guild.id) == undefined && message.content == '!robl') { 
     message.channel.send(`Канал не выбран. Админ должен написать !setchannel, чтобы выбрать этот канал`); return; }
@@ -34,7 +115,8 @@ client.on('message', message => {
   };
 
   switch (message.content.split(" ")[0]){
-    case "!colorme" :
+    case "!colorme":
+	if (!initInfo.permGuilds.includes(message.guild.id)) break;
     try{
       let clr = message.content.split(" ").pop(); if (clr.startsWith('#')) clr = clr.substr(1);
       if (clr === 'random') {
@@ -83,9 +165,10 @@ client.on('message', message => {
         description: `!robl - помощь
 !colorme [хекскод цвета](https://color-hex.com) или random - присвоить себе цвет
 !cleanme - удалить цветную роль
-!toggle ***полное имя роли*** - включить/выключить желаемую роль. Полное имя роли можно найти, начав вводить его после \"@\"`}});
+!toggle ***полное имя роли*** - включить/выключить желаемую роль. Полное имя роли можно найти, начав вводить его после \"@\". \"@"\ перед отправкой сообщения нужно убрать!`,
+}});
       break;
-      
+
 
 
       case "!toggle" :
@@ -97,10 +180,25 @@ client.on('message', message => {
         } else {
           mmbr.addRole(rlnmrole)
           .then(() => {message.channel.send(`${mmbr.displayName}, роль добавлена`);})
-          .catch(() => {message.channel.send(`${mmbr.displayName}, у меня нет прав на эту роль`); return })
+          .catch(() => {message.channel.send(`${mmbr.displayName}, у меня нет прав на эту роль`); return }) 
           }
         break;
   }  
 })
 
-client.login('token');
+function getVKLinks(arrPar)
+{
+  let _arr = [];
+  arrPar.forEach(element => {
+        element = element.match(/"[a-z]":"https.*?"/gm).sort();
+        if (element[0][1] === 'w') {
+          element = element[0].substr(5).replace(/[\\"]/g, '');
+        } else {
+          element = element.reverse()[0].substr(5).replace(/[\\"]/g, '');
+        }
+    _arr.push(element);
+  })
+  return _arr;
+}
+
+client.login(initInfo.key);
